ef17f1fee45bb93a45d3b7fe1069f5a072b8cbcf
YangDamin <cxz26@naver.com>
2022-05-31 11:56:18 +0900

    TodoItem 틀 생성

--- todolist/src/todolist/TodoList.js
+++ todolist/src/todolist/TodoList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import TodoItem from './TodoItem';
 
 const TodoListStyle = styled.div`
   flex: 1; /* 차지할 수 있는 영역을 꽉 채운다.*/
@@ -9,7 +10,14 @@ const TodoListStyle = styled.div`
 `;
 
 function TodoList() {
-  return <TodoListStyle>TodoList</TodoListStyle>;
+  return (
+    <TodoListStyle>
+      <TodoItem text="프로젝트 하기" done={true} />
+      <TodoItem text="운동하기" done={true} />
+      <TodoItem text="공부하기" done={false} />
+      <TodoItem text="놀러가기" done={false} />
+    </TodoListStyle>
+  );
 }
 
 export default TodoList;
